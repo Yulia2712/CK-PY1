a4a84ae5b117bbbcaba8b6dace57020ce446b75c
Yulia2712 <119407393+Yulia2712@users.noreply.github.com>
2022-11-29 12:26:27 +0300

    Лабораторная работа №3

---
+++ Лабораторная работа №3/task_6.py
@@ -0,0 +1,18 @@
+list_numbers = [2, 90, -2, 8, -36, -44, -1, -85, -14, 90, -22, -90, -100, -8, 38, -92, -45, 67, 53, 25]
+
+max_index = 0
+max_number = list_numbers[max_index]
+
+for i, current_number in enumerate(list_numbers):
+    max_number = list_numbers[max_index]
+    if current_number >= max_number:
+        max_index = i
+        max_number = list_numbers[max_index]
+
+last_number = list_numbers[-1]
+
+
+list_numbers[max_index], list_numbers[-1] = last_number, max_number
+# TODO Оформить решение
+
+print(list_numbers)  # Ответ [2, 90, -2, 8, -36, -44, -1, -85, -14, 25, -22, -90, -100, -8, 38, -92, -45, 67, 53, 90]
